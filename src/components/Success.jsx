7cd69f5b1d707636da84e2252260c202bb315e2e
Nicolas Gomez <nicolasgomez94@gmail.com>
2025-04-11 11:03:26 -0300

    ajustes varios

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,8 +1,14 @@
 const Success = () => (
-	<div style={{ textAlign: "center", marginTop: "50px", color: "white" }}>
+	<div style={{ textAlign: "center",
+			color: "white",
+			backgroundColor: "#333333de",
+			boxShadow: "0 0 10px 4px #1a1a1adb",
+			padding: "20px",
+			borderRadius: "10px",
+		}}>
 		<h2>🎉 ¡Gracias por tu reserva en BLAK!</h2>
 		<p>Tu pago fue aprobado y tu turno quedó confirmado.</p>
-        <a href="http://localhost:5173/">Volver al Calendario</a>
+        <a href="http://localhost:5173/">Volver a la App</a>
 	</div>
 );
 
